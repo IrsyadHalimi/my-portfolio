ba4cf9c3ffb4990f128c066dc7a7c97c3cc8ef42
IrsyadHalimi <irsyadhalimi08@gmail.com>
2023-12-26 22:01:01 +0700

    update 1/3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 $getJSON
+
+function openModal(imageName) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImg");
+
+  modal.style.display = "block";
+  modalImg.src = imageName;
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
